256893fb79592e81999e6ef98c529d70b6d9758d
rawataryan008 <rawataryan008@gmail.com>
2026-08-03 21:11:16 +0530

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.size()!=t.size())
+            return false;
+        int count[26]={0};
+        for(char c : s)
+            count[c-'a']++;
+        for(char c : t)
+            count[c-'a']--;
+        for(int x:count)
+        {
+            if(x>0)
+                return false;
+        }
+        return true;
+
+    }
+};
